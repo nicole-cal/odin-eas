4596966ca2215cf74fba6167dc8f1e72f5715402
nicole-cal <nicole.cal.73@gmail.com>
2022-06-19 11:56:11 +0100

    Add hover event listener

--- script.js
+++ script.js
@@ -1,45 +1,28 @@
-// Need to relate createGrid to DOM somehow.
-
-let canvas = document.getElementById("canvas");
-
-/* const example = document.querySelector('#example');
-const content = document.createElement('div');
-content.classList.add('content');
-content.textContent = createGrid();
-example.appendChild(content); */
-
-
-//let container = document.getElementById("grid"); ****
-let grid = document.getElementById("container");
-//let container = document.getElementById("cell");
-
-// How to get the ## below into the container or canvas?? Need to append it to that somehow??
-// Is the thing below locked into local scope?
-
-//function print(s) {document.getElementById('out').innerHTML += s;}
-//function println(s) {document.getElementById('out').innerHTML += s + '\n';}
-
+//let container = document.getElementById("grid"); this removes the grid
+//let grid = document.getElementById("container"); grid remains the same
 
     for (let i = 0; i < 16; i++) {
         for (let j = 0; j < 16; j++) {
           let cell = document.createElement("div");
           //cell.appendChild(canvas);
           //print("#");
-          cell.innerHTML = "##"; // need to get this to do a div. Doesn't insert DOM nodes just strings
+          cell.innerHTML = ""; // need to get this to do a div. Doesn't insert DOM nodes just strings
           cell.className = "cell";
           container.appendChild(cell);
-        }
+
+          cell.addEventListener("mouseenter", func, false);
+          function func() {
+            cell.setAttribute("style", "background: magenta;");
+          }
+       }
     }
   
+    //let hover = document.getElementById("container"); // works on grid using grid as ID
+    // how to get from grid to cell?
   
+    //hover.addEventListener("mouseenter", func, false);
 
-
+    //function func() {
+    //hover.setAttribute("style", "background: magenta;");
+    //}  
   
-
-/* let canvas = document.querySelector('#canvas');
-canvas.appendChild(container); */
-
-//createGrid();
-
-
-
