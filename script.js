20ebea2a859c5eacb36e007028281608a622e27f
nicole-cal <nicole.cal.73@gmail.com>
2022-06-20 11:33:42 +0100

    Add calculation to change cell size depending on input

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
 //let container = document.getElementById("grid"); this removes the grid
 //let grid = document.getElementById("container"); grid remains the same
 
-    for (let i = 0; i < 16; i++) {
-        for (let j = 0; j < 16; j++) {
-          let cell = document.createElement("div");
-          //cell.appendChild(canvas);
-          //print("#");
-          cell.innerHTML = ""; // need to get this to do a div. Doesn't insert DOM nodes just strings
-          cell.className = "cell";
-          container.appendChild(cell);
-
-          cell.addEventListener("mouseenter", func, false);
-          function func() {
-            cell.setAttribute("style", "background: magenta;");
-          }
-       }
-    }
-  
   function newGrid() {
-    let howManySquares = prompt("How many squares would you like each side of the grid to be? \nFor example, entering 8 will produce an 8 x 8 grid.");
-    alert(howManySquares);
-  };
+    let howManySquares = prompt("How many squares would you like each side of the grid to be, between 1 and 100? \nFor example, entering 8 will produce an 8 x 8 grid.");
+    alert("Make a way to close this automatically");
+
+    let pixelSizeCell = ((960 - [howManySquares * 4]) / howManySquares); // Calculation takes into account cell border: 2px on each side.
+
+    for (let i = 0; (i < howManySquares && howManySquares < 100); i++) {
+      for (let j = 0; (j < howManySquares && howManySquares < 100); j++) {
+      
+        let cell = document.createElement("div");
+        cell.innerHTML = ""; 
+        cell.className = "cell";
+        container.appendChild(cell);
+        cell.style.width = `${pixelSizeCell}px`;
+        cell.style.height = `${pixelSizeCell}px`;
+
+         cell.addEventListener("mouseenter", func, false);
+        function func() {
+          cell.setAttribute("style", "background: magenta;");
+          cell.style.width = `${pixelSizeCell}px`;
+         cell.style.height = `${pixelSizeCell}px`;
+        }  
+     } 
+  }
+};
+
+// want a MAXIMUM of 100.
